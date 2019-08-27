51a31261a889c6963eaa94fd1bd3dd69bf272094
DRIMchansky <drimchansky@gmail.com>
2019-08-27 17:07:19 +0300

    initial prepare

--- baker/app/js/_custom.js
+++ baker/app/js/_custom.js
@@ -1,5 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
+const hamburger = document.querySelector('#hamburger');
+const menu = document.querySelector('#menu');
 
-	// Custom JS
 
+const hamburgerToggle = () => {
+    hamburger.classList.toggle('is-active');
+    menu.classList.toggle('visible');
+    hamburger.style.outline = 'none';
+};
+
+
+// listners
+hamburger.addEventListener('click', hamburgerToggle);
+document.addEventListener('keydown', (press) => {
+    if (press.keyCode === 27) {
+        hamburger.classList.remove('is-active');
+        menu.classList.remove('visible');
+    }
 });
