668c7cc30a3c83a69a8c4b1d8f0448593647cb1a
DRIMchansky <drimchansky@gmail.com>
2019-09-06 19:01:01 +0300

    card mobile (one)

--- baker/dist/main.d2070829.js
+++ baker/dist/main.d2070829.js
@@ -280,7 +280,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"styles/main.css":[function(require,module,exports) {
+},{"./..\\..\\img\\cake.png":[["cake.4818ee92.png","img/cake.png"],"img/cake.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"styles/main.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -313,7 +313,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "21534" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "23071" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
