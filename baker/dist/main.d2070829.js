91424dc3e0232a1f09972ef092b5819bf122480c
DRIMchansky <drimchansky@gmail.com>
2019-09-07 10:58:27 +0300

    stats mobile

--- baker/dist/main.d2070829.js
+++ baker/dist/main.d2070829.js
@@ -328,7 +328,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "20244" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "20332" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
